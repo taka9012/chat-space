d64785be8ca04ae7229dee3926879f2217da9443
taka9012 <u.hide1201@icloud.com>
2020-04-28 10:19:24 +0900

    edit fog env value

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,9 +2,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-
+  process resize_to_fit: [800, 800]
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
-  process resize_to_fit: [800, 800]
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
